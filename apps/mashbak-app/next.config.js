60bd3deaf097413d81c76454c3f684805121f784
eliassalom <elias.29@hotmail.com>
2025-09-17 21:36:49 +0300

    feat: implement authentication routes and user management with Prisma integration

--- apps/mashbak-app/next.config.js
+++
@@ -1,7 +0,0 @@
-// import type { NextConfig } from "next";
-
-const nextConfig = {
-  /* config options here */
-};
-
-module.exports = nextConfig;
--- apps/mashbak-app/src/components/NavBar/type.ts
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-
-export interface INavBarPage {
-  label: string;
-  path: string;
-  icon?: ReactElement;
-}
