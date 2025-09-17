46582a22cb72f6fe0aa7e3b31fcfb6da9a3e792f
eliassalom <elias.29@hotmail.com>
2025-09-18 02:13:24 +0300

    refactor: reorganize imports and enhance AuthModule with HttpModule and JwtModule configuration

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthDao } from './auth.dao';
+import { PrismaClient } from '@prisma/client';
+import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthDao, PrismaClient],
 })
 export class AuthModule {}
