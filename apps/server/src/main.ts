58d5e108bc3b5aea055f392bb634122159ed8c76
eliassalom <elias.29@hotmail.com>
2025-09-18 18:48:39 +0300

    refactor: format imports in auth.dao.ts and enable CORS in main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   const PORT = process.env.PORT || 3001;
   await app.listen(PORT, '0.0.0.0');
 }
