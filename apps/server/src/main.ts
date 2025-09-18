325413db4e1a597a9cd235a356d116451529dffd
eliassalom <elias.29@hotmail.com>
2025-09-18 16:42:26 +0300

    refactor: reorganize imports and enhance AuthModule with HttpModule and JwtModule configuration

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3001;
-  await app.listen(PORT);
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
