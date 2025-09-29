7ad4f2154fd541b54598db0d7bf6d0c186da08eb
eliassalom <elias.29@hotmail.com>
2025-09-29 10:01:21 +0300

    refactor: update Vite configuration to enable host and allowedHosts options for preview

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import {defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 
 interface Prop {
@@ -14,6 +14,8 @@ export default ({ mode }: Prop) => {
     },
     preview: {
       port: parseInt(process.env.VITE_PORT as string),
+        allowedHosts: true,
+        host: true,
     },
   });
 };
